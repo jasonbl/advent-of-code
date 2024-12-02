4831b6730d330726833fe1530b7cf8f5084ec411
Jason Bleiweiss <jason.bleiweiss@gmail.com>
2024-12-01 21:53:09 -0500

    Day 1

--- init_solution.sh
+++ init_solution.sh
@@ -6,22 +6,24 @@ if [ "$#" -ne 2 ]; then
 fi
 
 package="year$1/day$2"
+
 solution_path="src/main/java/$package"
+solution_name="Solution.kt"
 input_path="src/main/resources/$package"
+input_name="input.txt"
+
+solution_template="package year$1.day$2
+
+import util.InputLoader
+
+fun main() {
+  val input = InputLoader.load(\"/$package/$input_name\")
+}"
 
 mkdir -p "$solution_path"
 mkdir -p "$input_path"
 
-solution_template="package year$1.day$2
-
-class Solution {
-
-  fun main() {
-
-  }
-}"
-
-echo "$solution_template" > "$solution_path/Solution.kt"
-touch "$input_path/input.txt"
+echo "$solution_template" > "$solution_path/$solution_name"
+touch "$input_path/$input_name"
 
 git add .
---
+++ src/main/java/year2024/day1/Solution.kt
@@ -0,0 +1,41 @@
+package year2024.day1
+
+import util.InputLoader
+import kotlin.math.abs
+
+val INPUT_REGEX = Regex("(\\d+)\\s+(\\d+)")
+
+fun main() {
+  val inputs = InputLoader.load("/year2024/day1/input.txt")
+    .split("\n")
+
+  val matchResults = inputs.map { INPUT_REGEX.matchEntire(it)!! }
+
+  val leftList = Array(inputs.size) { matchResults[it].groupValues[1].toInt() }
+  val rightList = Array(inputs.size) { matchResults[it].groupValues[2].toInt() }
+
+  println("Part 1: " + partOne(leftList, rightList))
+  println("Part 2: " + partTwo(leftList, rightList))
+}
+
+fun partOne(leftList: Array<Int>, rightList: Array<Int>): Int {
+  leftList.sort()
+  rightList.sort()
+
+  var diff = 0
+  for (i in leftList.indices) {
+    diff += abs(rightList[i] - leftList[i])
+  }
+
+  return diff
+}
+
+fun partTwo(list1: Array<Int>, list2: Array<Int>): Int {
+  val counts = HashMap<Int, Int>()
+  list2.forEach {
+    val currCount = counts.getOrDefault(it, 0)
+    counts[it] = currCount + 1
+  }
+
+  return list1.sumOf { it * counts.getOrDefault(it, 0) }
+}
